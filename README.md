71110c71b812f9bbf3bfe76673934716ae729d13
dana <a01658253@tec.mx>
2024-03-21 15:49:21 -0600

    Agregando un color y un círculo

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Act1_Paint
 Actividad 1. Juego Pintando Semana
 #Codigo original de paint.py
+#Agregando un color y círculo
--- paint.py
+++ paint.py
@@ -1,18 +1,6 @@
-"""Paint, for drawing shapes.
-
-Exercises
-
-1. Add a color.
-2. Complete circle.
-3. Complete rectangle.
-4. Complete triangle.
-5. Add width parameter.
-"""
-
 from turtle import *
-
 from freegames import vector
-
+import math
 
 def line(start, end):
     """Draw line from start to end."""
@@ -20,7 +8,6 @@ def line(start, end):
     goto(start.x, start.y)
     down()
     goto(end.x, end.y)
-
 
 def square(start, end):
     """Draw square from start to end."""
@@ -35,24 +22,24 @@ def square(start, end):
 
     end_fill()
 
-
-def circle(start, end):
+def draw_circle(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
-
+    up()
+    goto(start.x, start.y)
+    down()
+    radius = math.sqrt((end.x - start.x) * 2 + (end.y - start.y) * 2)
+    circle(radius)
 
 def rectangle(start, end):
-    """Draw rectangle from start to end."""
-    pass  # TODO
-
+    "Draw rectangle from start to end."
+    pass  #TODO
 
 def triangle(start, end):
-    """Draw triangle from start to end."""
-    pass  # TODO
-
+    "Draw triangle from start to end."
+    pass  #TODO
 
 def tap(x, y):
-    """Store starting point or draw shape."""
+    "Store starting point or draw shape."
     start = state['start']
 
     if start is None:
@@ -63,10 +50,18 @@ def tap(x, y):
         shape(start, end)
         state['start'] = None
 
-
 def store(key, value):
-    """Store value in state at key."""
+    "Store value in state at key."
     state[key] = value
+
+state = {'start': None, 'shape': line}
+setup(420, 420, 370, 0)
+onscreenclick(tap)
+listen()
+onkey(undo, 'u')
+# Define a function to change color to purple
+def set_purple():
+    color('purple')
 
 
 state = {'start': None, 'shape': line}
@@ -79,9 +74,10 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(set_purple, 'P')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', draw_circle), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
 done()
